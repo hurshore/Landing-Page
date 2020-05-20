792c005646bf960bd6daded9118d98449d0089a1
Tofunmi Osho <oshotofunmi@gmail.com>
2020-05-20 15:34:22 +0100

    Mobile view done

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const hamburgerIcon = document.querySelector('.nav-open');
+const closeIcon = document.querySelector('.nav-close');
+const responsiveNav = document.querySelector('.responsive-nav');
+const backdrop = document.querySelector('.backdrop');
+let open = false;
+
+hamburgerIcon.addEventListener('click', navToggle);
+closeIcon.addEventListener('click', navToggle);
+
+function navToggle(event) {
+  console.log(event.target.parentElement);
+  if(event.target.parentElement.classList.contains('nav-open')){
+    event.target.parentElement.style.display = 'none';
+    closeIcon.style.display = 'block';
+    responsiveNav.style.display = 'block';
+    open = true;
+    backdrop.style.display = 'block';
+  } else if(event.target.parentElement.classList.contains('nav-close')){
+    event.target.parentElement.style.display = 'none';
+    hamburgerIcon.style.display = 'block';
+    responsiveNav.style.display = 'none';
+    backdrop.style.display = 'none';
+    open = false;
+  }
+  event.preventDefault();
+}
